55ed2038d949de8715afeec400a83317f7175005
mieses <manuel.mieses48@gmail.com>
2018-03-14 21:46:11 -0400

    creacion de vistalis compani

--- inventario/views.py
+++ inventario/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import compania
 
 # Create your views here.
 def compania_list(request):
-    return render(request, 'inventario/compania_list.html', {})
+    Compania = compania.objects.all()
+    return render(request, 'inventario/compania_list.html', {'Compania' : Compania})
