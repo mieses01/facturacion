4d8d6e3c375ee093051475ff3e81a131b372c44d
mieses <manuel.mieses48@gmail.com>
2018-03-26 23:20:58 -0400

    urls y vista

--- inventario/urls.py
+++ inventario/urls.py
@@ -2,6 +2,11 @@ from django.conf.urls import include, url
 from .import views
 from django.urls import path
 
+app_name = 'inventario'
+
 urlpatterns = [
     path('',  views.compania_list),
+    path('<int:compania_id>/', views.compania_detail, name='compania_detail'),
+    
+
 ]
--- inventario/views.py
+++ inventario/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from .models import compania
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
 
 # Create your views here.
 def compania_list(request):
     Compania = compania.objects.all()
     return render(request, 'inventario/compania_list.html', {'Compania' : Compania})
+
+class detail(generic.DetailView):
+    model = compania
+    template_name = 'inventario/compania_detail.html'
+
+def compania_detail(request, compania_id):
+    
+    Compania = get_object_or_404(compania, pk=compania_id)
+    return render(request, 'inventario/compania_detail.html', {'Compania': Compania})
